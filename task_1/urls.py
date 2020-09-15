7b8fd6aadf995ee0239659e9219e79314d7c9a3f
Heba <36724067+heba@users.noreply.github.com>
2020-09-15 10:28:24 +0300

    add create api view

--- task_1/urls.py
+++ task_1/urls.py
@@ -16,13 +16,25 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from flights import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+
     path('flights/', views.FlightsList.as_view(), name="flights-list"),
-    
-    path('bookings/', views.BookingsList.as_view(), name="bookings-list"), 
+
+    path('bookings/', views.BookingsList.as_view(), name="bookings-list"),
     path('booking/<int:booking_id>/', views.BookingDetails.as_view(), name="booking-details"),
     path('booking/<int:booking_id>/update/', views.UpdateBooking.as_view(), name="update-booking"),
     path('booking/<int:booking_id>/cancel/', views.CancelBooking.as_view(), name="cancel-booking"),
+
+    path('booking/<int:flight_id>/book/', views.CreateBooking.as_view(), name="book-flight"),
+
+
 ]
